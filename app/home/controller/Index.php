35fc2889b497bc9948d2a3354c90ada96059092c
1452073959@qq.com <jok125890>
2022-05-11 17:51:12 +0800

    1

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -2,6 +2,7 @@
 declare (strict_types = 1);
 
 namespace app\home\controller;
+use app\home\help\Pdd;
 use app\home\help\Result;
 use app\HomeController;
 class Index extends HomeController
@@ -9,6 +10,24 @@ class Index extends HomeController
     public function index()
     {
       return Result::Success('a');
+    }
+
+
+    public function pdd()
+    {
+        $req = request()->param();
+        $arr=[
+            'type'=>'pdd.ddk.goods.recommend.get',
+            'client_id'=>'2e46e1e0b1394538960222e5fb1b9009',
+            'timestamp'=>time(),
+            'activity_tags'=>'[4,7]',
+        ];
+        $arr= array_merge($req,$arr);
+//        dump($arr);
+        $feemypay = new Pdd();
+        $a=  $feemypay->ceshi('pdd.ddk.goods.recommend.get',$arr);
+
+        return $a;
     }
 
     public function team()

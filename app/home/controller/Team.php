3948b674e571ac18f609779b04f69feb1985c0c6
1452073959@qq.com <jok125890>
2022-05-23 11:09:40 +0800

    1

--- app/home/controller/Team.php
+++ app/home/controller/Team.php
@@ -9,6 +9,7 @@
 namespace app\home\controller;
 
 use app\admin\model\admin\User;
+use app\admin\model\plan\PlanDeal;
 use app\admin\model\UserCard;
 use app\home\help\Result;
 use app\HomeController;
@@ -39,10 +40,10 @@ class Team extends HomeController
         $Internship_z_v3 = User::where('pid', $user['id'])->where('vip_label', '4')->count();
         $Internship_jz_v3 = User::where('pid', '<>', $user['id'])->where('vip_label', '4')->whereIn('id', $a)->count();
         $vip = [];
-        $vip['sx'] = ['zt'=>$Internship_z, 'jt'=>$Internship_j];
-        $vip['v1'] = ['zt'=>$Internship_z_v1, 'jt'=>$Internship_jz_v1];
-        $vip['v2'] = ['zt'=>$Internship_z_v2, 'jt'=>$Internship_jz_v2];
-        $vip['v3'] = ['zt'=>$Internship_z_v3, 'jt'=>$Internship_jz_v3];
+        $vip['sx'] = ['zt' => $Internship_z, 'jt' => $Internship_j];
+        $vip['v1'] = ['zt' => $Internship_z_v1, 'jt' => $Internship_jz_v1];
+        $vip['v2'] = ['zt' => $Internship_z_v2, 'jt' => $Internship_jz_v2];
+        $vip['v3'] = ['zt' => $Internship_z_v3, 'jt' => $Internship_jz_v3];
 
         return Result::Success(['teamnum' => $teamnum, 'directly' => $Directly, 'real_name' => $real_name, 'vip' => $vip]);
     }
@@ -66,10 +67,10 @@ class Team extends HomeController
         $lastdaycard = User::whereDay('create_time', 'yesterday')->whereIn('id', $a)->count();
 
         return Result::Success(['month' => $Month, 'lastmonth' => $lastMonth, 'day' => $day, 'lastday' => $lastday,
-        'Monthcard'=>$Monthcard,
-        'lastcard'=>$lastcard,
-        'daycard'=>$daycard,
-        'lastdaycard'=>$lastdaycard,
+            'Monthcard' => $Monthcard,
+            'lastcard' => $lastcard,
+            'daycard' => $daycard,
+            'lastdaycard' => $lastdaycard,
         ]);
     }
 
@@ -85,9 +86,8 @@ class Team extends HomeController
             ];
         };
         $directlyteam = User::where('pid', $user['id'])->where($where)->paginate(10)->toArray();
-        foreach ($directlyteam['data'] as $k=>$v)
-        {
-            $directlyteam['data'][$k]['teamnum']=User::where('pid', $v['id'])->count();
+        foreach ($directlyteam['data'] as $k => $v) {
+            $directlyteam['data'][$k]['teamnum'] = User::where('pid', $v['id'])->count();
         }
         return Result::Success($directlyteam);
     }
@@ -112,9 +112,8 @@ class Team extends HomeController
             ->where('vip_label', $req['vip_label'])
             ->paginate(10)->toArray();
 
-        foreach ($member['data'] as $k=>$v)
-        {
-            $member['data'][$k]['teamnum']=User::where('pid', $v['id'])->count();
+        foreach ($member['data'] as $k => $v) {
+            $member['data'][$k]['teamnum'] = User::where('pid', $v['id'])->count();
         }
         return Result::Success($member);
 
@@ -124,9 +123,68 @@ class Team extends HomeController
     //会员详情
     public function member_show()
     {
+        $user = $this->user(request());
         $req = request()->param();
-        //收款当月
+        //还款当月/上月
+        $hk1 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->where('user_id', $user['id'])
+            ->whereMonth('trade_time')
+            ->sum('actual_amount');
+        $hk2 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->where('user_id', $user['id'])
+            ->whereMonth('trade_time', 'last month')
+            ->sum('actual_amount');
+        //新增直推好友当月/上月
+        $zt1 = User::where('pid', $user['id'])->whereMonth('create_time')->count();
+        $zt2 = User::where('pid', $user['id'])->whereMonth('create_time', 'last month')->count();
+        //新增直推v1
+        $ztv1 = User::where('pid', $user['id'])->whereMonth('create_time')->where('vip_label',2)->count();
+        $ztv2 = User::where('pid', $user['id'])->whereMonth('create_time', 'last month')->where('vip_label',2)->count();
+        //获取团队(不包含自己)
+        $a = GetTeamMember($user['id']);
+        //新增直推好友昨日/当月/上月/累计
+        $tzt1 = User::whereDay('create_time', 'yesterday')->whereIn('id', $a)->count();
+        $tzt2 = User::whereMonth('create_time')->whereIn('id', $a)->count();
+        $tzt3 = User::whereMonth('create_time', 'last month')->whereIn('id', $a)->count();
+        $tzt4 = User::whereIn('id', $a)->count();
 
+        //新增直推v1
+        $tztv1 = User::whereDay('create_time', 'yesterday')->where('vip_label',2)->whereIn('id', $a)->count();
+        $tztv2 = User::whereMonth('create_time')->where('vip_label',2)->whereIn('id', $a)->count();
+        $tztv3 = User::whereMonth('create_time', 'last month')->where('vip_label',2)->whereIn('id', $a)->count();
+        $tztv4 = User::where('vip_label',2)->whereIn('id', $a)->count();
+        //交易量
+        $tzthk1 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->whereIn('id', $a)
+            ->whereDay('trade_time','yesterday')
+            ->sum('actual_amount');
+        $tzthk2 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->whereIn('id', $a)
+            ->whereMonth('trade_time')
+            ->sum('actual_amount');
+        $tzthk3 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->whereIn('id', $a)
+            ->whereMonth('trade_time', 'last month')
+            ->sum('actual_amount');
+        $tzthk4 = PlanDeal::where('trade_type', 2)
+            ->where('trade_status', 3)
+            ->whereIn('id', $a)
+            ->sum('actual_amount');
+
+        return Result::Success([
+            'hk'=>['month'=>$hk1,'last_month'=>$hk2],
+            'zt'=>['month'=>$zt1,'last_month'=>$zt2],
+            'ztv1'=>['month'=>$ztv1,'last_month'=>$ztv2],
+            'team_zt'=>['month'=>$tzt2,'last_month'=>$tzt3,'yesterday'=>$tzt1,'total'=>$tzt4],
+            'team_ztv1'=>['month'=>$tztv2,'last_month'=>$tztv3,'yesterday'=>$tztv1,'total'=>$tztv4],
+            'team_jy'=>['month'=>$tzthk2,'last_month'=>$tzthk3,'yesterday'=>$tzthk1,'total'=>$tzthk4],
+
+        ],'成功');
     }
 
 }
